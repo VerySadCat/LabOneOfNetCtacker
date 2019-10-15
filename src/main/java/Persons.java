1d654665e97388b7ccd84cfec8d89a80cebabf3a
User <s>
2019-10-15 10:53:41 +0300

    Initial commit

---
+++ src/main/java/Persons.java
@@ -0,0 +1,35 @@
+import date.Date;
+
+/**
+ *
+ */
+public class Persons {
+    private Date birtday;
+    private char gender;
+    private float growth;
+
+    public Date getBirtday() {
+        return birtday;
+    }
+
+    public void setBirtday(Date birtday) {
+        this.birtday = birtday;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public float getGrowth() {
+        return growth;
+    }
+
+    public void setGrowth(float growth) {
+        this.growth = growth;
+    }
+
+}
