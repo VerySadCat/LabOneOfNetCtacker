f33f4ca4c0d9bc8410f5edad27428ce76267fb3b
User <s>
2019-10-15 22:37:59 +0300

    add new javadoc            add package

--- src/main/java/MyArray/MyArray.java
+++ src/main/java/MyArray/MyArray.java
@@ -1,10 +1,33 @@
 package MyArray;
 
-import Persons.Persons;
+import persons.Persons;
 
 public class MyArray {
 
     private Persons array[];
-    private int cardinality;
+    private int cardinality = 0;
+
+    public void add(Persons value){
+        cardinality++;
+        if (array == null){
+            array = new Persons[cardinality];
+        }
+        else{
+            Persons[] newArray = new Persons[cardinality];
+            newArray = array.clone();
+            array = newArray;
+        }
+         array[cardinality-1] = value;
+    }
+
+    public void printInformPerson(){
+        for (Persons temp : array ) {
+            System.out.println( temp.getName() + " "
+                    + temp.getBirtday().getDay() + "." + temp.getBirtday().getMonth() + "." + temp.getBirtday().getYear() + " "
+                    + temp.getGender() + " "
+                    + temp.getGrowth()
+            );
+        }
+    }
 
 }
--- src/main/java/persons/Persons.java
+++ src/main/java/persons/Persons.java
@@ -1,4 +1,4 @@
-package Persons;
+package persons;
 
 import date.Date;
 
